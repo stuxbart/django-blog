f4476aa0e9ff786eddfb2c53ba7ebe00172c3ea0
stuxbart <stuxbart99@gmail.com>
2020-08-23 14:44:46 +0200

    Add comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    search_fields = ('name', 'body')
+    list_filter = ('active', 'created')
+
+
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -37,3 +37,19 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', kwargs={'slug': self.slug})
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return f"{self.name} add a comment to the '{self.post}' post"
